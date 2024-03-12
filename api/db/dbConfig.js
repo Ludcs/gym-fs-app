44db941c5b16443f73372640d8db50d03d7a115b
Luciano De Carolis <parodia452@gmail.com>
2024-03-12 12:28:38 -0300

    setting vars for pre-deploy

--- api/db/dbConfig.js
+++ api/db/dbConfig.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('GymDb', 'root', process.env.PASSDBCONFIG, {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: console.log,
-});
+const sequelize = new Sequelize(
+  process.env.NAMEDB,
+  process.env.USERNAMEDB,
+  process.env.PASSDBCONFIG,
+  {
+    host: process.env.HOST,
+    dialect: 'mysql',
+    port: process.env.DBPORT,
+    logging: console.log,
+  }
+);
 
 module.exports = sequelize;
